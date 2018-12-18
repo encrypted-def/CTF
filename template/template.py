20872cc345e7c415ac58d5f7fe1201be11e02c92
blisstoner <blisstoner@gmail.com>
2018-12-19 04:14:52 +0900

    Update template.py

--- template/template.py
+++ template/template.py
@@ -1,9 +1,5 @@
-# 0x6161 -> 'AA'
-def hexint2str(x):
-  import binascii
-  s = hex(x)[2:]
-  if len(s) % 2 == 1: s = '0'+s
-  return binascii.unhexlify(s).decode()
+from Crypto.Util.number import *
+import binascii,socket,sys
 
 def gcd(a, b):
   if a == 0: return b
@@ -25,7 +21,6 @@ def inv(a, m):
 def quad_congruence_equation(a, m):
   assert((m+1)%4 == 0)
   return pow(a, (m+1)//4, m)
-
 # m must satisfies pairwise relatively prime
 def crt(a, m):
   n = len(m)
@@ -38,3 +33,32 @@ def crt(a, m):
     m1inv = inv(m1,m[i])
     ret = (ret*m[i]*m2inv+a[i]*m1*m1inv)%mod
   return ret
+
+# 0x6161 -> 'AA'
+def i2s(x):
+  return long_to_bytes(x).decode("utf-8")
+
+############ my socket ###############
+def interactive(socket):
+  print("[+] interactive mode")
+  while True:
+    rr = socket.recv(2**16).decode()
+    if not rr:
+      print("[!] socket closed")
+      return None
+    print(rr)
+    socket.send((input('> ')+'\n').encode())
+
+def remote(ip, port):
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  print("[+] Connecting to {}:{}".format(ip,port))
+  sock.connect((ip,port))
+  print("[+] Done!")
+  return sock
+
+def sendline(socket, msg):
+  if type(msg) == str: msg = msg.encode()
+  if msg[-1] != b'\n': msg += b'\n'
+  socket.send(msg)
+
+###################################
